4f76ccf93b66a14ec9e3c10e6bd859e148c44c5a
LeBogo <lebogomc@gmail.com>
2025-01-29 00:29:28 +0100

    refactor board-rendering related logic into separate component to cleanup code

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import { images, goals } from "./data.js";
   import { Rng } from "./lib/rng.js";
   import ThemeToggle from "./lib/ThemeToggle.svelte";
+  import Board from "./lib/Board.svelte";
 
   let userSeed =
     parseInt(localStorage.getItem("seed")) ||
@@ -22,54 +23,14 @@
     })()
   );
 
-  // pick the first 24 goals
-  let board = $derived(randomGoals.slice(0, 25));
+  let fringoBoard: Board | null = null;
 
+  const board = $derived(randomGoals.slice(0, 25));
   let image = $state(images[Math.floor(rng.next() * images.length)]);
-
-  let checkedData = JSON.parse(localStorage.getItem("checkedData"));
-
-  if (!checkedData || checkedData.date !== now) {
-    checkedData = {
-      date: now,
-      checked: new Array(25).fill(false).map((_, i) => i == 12),
-    };
-  }
-
-  let checked = $state(checkedData.checked); // one extra false for the center
-  $effect(() => {
-    localStorage.setItem("checkedData", JSON.stringify({ checked, date: now }));
-  });
-
-  // Ensure the center image is always checked and can't be unchecked
-
   let isFringo = $state(false);
-  let fringoCount = $state(0);
-
-  let fringoIndexes = $state([]);
-
   let headingText = $state("Fringo");
-  let headingCharacters = $derived(headingText.split(""));
+  const headingCharacters = $derived(headingText.split(""));
 
-  $effect(() => {
-    if (isFringo) {
-      headingText = `It's a ${toMultiplier(fringoCount)}Fringo!`;
-    } else {
-      headingText = "Fringo";
-    }
-  });
-
-  function toMultiplier(amount: number): string {
-    console.log(amount);
-
-    if (amount === 1) return "";
-    if (amount === 2) return "double ";
-    if (amount === 3) return "triple ";
-    if (amount === 4) return "quadruple ";
-    return "";
-  }
-
-  // Function to update favicon
   function updateFavicon() {
     const link = (document.querySelector("link[rel~='icon']") ||
       document.createElement("link")) as HTMLLinkElement;
@@ -77,79 +38,6 @@
     link.href = image;
     document.getElementsByTagName("head")[0].appendChild(link);
   }
-
-  // Ensure "Gay" is always checked and can't be unchecked
-  let gayIndex = $derived(board.findIndex((goal) => goal === "Gay"));
-  $effect(() => {
-    if (gayIndex !== -1) {
-      checked[gayIndex] = true;
-    }
-  });
-
-  function toggleField(index: number) {
-    if (index === gayIndex) return; // Prevent toggling "Gay" field
-
-    checked[index] = !checked[index];
-
-    checkFringos();
-  }
-
-  function checkFringos() {
-    isFringo = false;
-    fringoCount = 0;
-    fringoIndexes = [];
-
-    for (let col = 0; col < 5; col++) {
-      for (let row = 0; row < 5; row++) {
-        // check if that field is already in fringoIndexes
-        if (fringoIndexes.includes(row * 5 + col)) continue;
-
-        // check row
-        if (
-          checked[row * 5] &&
-          checked[row * 5 + 1] &&
-          checked[row * 5 + 2] &&
-          checked[row * 5 + 3] &&
-          checked[row * 5 + 4]
-        ) {
-          isFringo = true;
-          fringoCount++;
-          [row * 5, row * 5 + 1, row * 5 + 2, row * 5 + 3, row * 5 + 4].forEach(
-            (i) => fringoIndexes.push(i)
-          );
-        }
-
-        // check column
-        if (
-          checked[col] &&
-          checked[col + 5] &&
-          checked[col + 10] &&
-          checked[col + 15] &&
-          checked[col + 20]
-        ) {
-          isFringo = true;
-          fringoCount++;
-          [col, col + 5, col + 10, col + 15, col + 20].forEach((i) =>
-            fringoIndexes.push(i)
-          );
-        }
-      }
-    }
-
-    // check diagonals
-    if (checked[0] && checked[6] && checked[12] && checked[18] && checked[24]) {
-      isFringo = true;
-      fringoCount++;
-      [0, 6, 12, 18, 24].forEach((i) => fringoIndexes.push(i));
-    }
-
-    if (checked[4] && checked[8] && checked[12] && checked[16] && checked[20]) {
-      isFringo = true;
-      fringoCount++;
-      [4, 8, 12, 16, 20].forEach((i) => fringoIndexes.push(i));
-    }
-  }
-  checkFringos();
 
   updateFavicon();
 
@@ -164,14 +52,28 @@
     randomGoals.splice(12, 0, "");
 
     image = images[Math.floor(rng.next() * images.length)];
-
-    checked = new Array(25).fill(false).map((_, i) => i == 12);
-
     isFringo = false;
-    fringoCount = 0;
-    fringoIndexes = [];
+
+    fringoBoard?.reset();
+
+    headingText = "Fringo";
 
     updateFavicon();
+  }
+
+  function onFringo(amount: number) {
+    let multipliers = {
+      1: "",
+      2: "Double ",
+      3: "Triple ",
+      4: "Quadruple ",
+    };
+
+    let multiplier = multipliers[amount];
+
+    isFringo = amount != 0;
+    if (isFringo) headingText = `It's a ${multiplier}Fringo!`;
+    else headingText = "Fringo";
   }
 </script>
 
@@ -187,39 +89,19 @@
       <span
         class:space={char === " "}
         class:fringo={isFringo && char !== " "}
-        style="animation-delay: {index * 0.1}s;">{char}</span
+        style="animation-delay: {index * 0.1}s;"
       >
+        {char}
+      </span>
     {/each}
   </h1>
-  <div class="board">
-    {#each board.slice(0, 12) as text, index (text)}
-      <button
-        disabled={isFringo && !fringoIndexes.includes(index)}
-        class="field"
-        onclick={() => toggleField(index)}
-        class:checked={checked[index]}
-        class:gay={index == gayIndex}
-      >
-        {text}
-      </button>
-    {/each}
-    <img src={image} alt="Funny Fringo" class="field" />
-    {#each board.slice(13) as text, index (text)}
-      <button
-        disabled={isFringo && !fringoIndexes.includes(index + 13)}
-        class="field"
-        onclick={() => toggleField(index + 13)}
-        class:checked={checked[index + 13]}
-        class:gay={index + 13 == gayIndex}
-      >
-        {text}
-      </button>
-    {/each}
-  </div>
+  <Board bind:this={fringoBoard} {board} {onFringo} {image} />
   <button
     id="new-game-button"
     class="btn btn-outline"
     class:hidden={!isFringo}
-    onclick={() => newBoard()}>New Board</button
+    onclick={() => newBoard()}
   >
+    New Board
+  </button>
 </div>
---
+++ src/lib/Board.svelte
@@ -0,0 +1,142 @@
+<script lang="ts">
+  const { board, onFringo, image } = $props();
+
+  const now = Math.floor(Date.now() / 1000 / 60 / 60 / 24);
+
+  let savedState: boolean[] = [];
+  let savedDay = localStorage.getItem("saveDay");
+  if (savedDay && parseInt(savedDay, 36) == now) {
+    savedState = JSON.parse(localStorage.getItem("checked"));
+  } else {
+    localStorage.removeItem("checked");
+    localStorage.removeItem("saveDay");
+  }
+
+  let checked = $state(
+    savedState.length
+      ? savedState
+      : new Array(25).fill(false).map((_, i) => i == 12 || board[i] == "Gay")
+  );
+  let isFringo = $state(false);
+  let fringoCount = $state(0);
+  let fringoIndexes = $state<number[]>([]);
+
+  function checkFringo() {
+    isFringo = false;
+    fringoIndexes = [];
+    fringoCount = 0;
+
+    for (let row = 0; row < 5; row++) {
+      if (checkRow(row)) {
+        isFringo = true;
+        fringoCount++;
+        for (let i = 0; i < 5; i++) {
+          fringoIndexes.push(row * 5 + i);
+        }
+      }
+    }
+
+    for (let col = 0; col < 5; col++) {
+      if (checkCol(col)) {
+        isFringo = true;
+        fringoCount++;
+        for (let i = 0; i < 5; i++) {
+          fringoIndexes.push(col + i * 5);
+        }
+      }
+    }
+
+    if (checkDiagonal()) {
+      isFringo = true;
+      fringoCount++;
+      [0, 6, 18, 24].forEach((i) => fringoIndexes.push(i));
+    }
+
+    if (checkAntiDiagonal()) {
+      isFringo = true;
+      fringoCount++;
+      [4, 8, 16, 20].forEach((i) => fringoIndexes.push(i));
+    }
+
+    onFringo(fringoCount);
+  }
+
+  function checkRow(row: number) {
+    return (
+      checked[row * 5] &&
+      checked[row * 5 + 1] &&
+      checked[row * 5 + 2] &&
+      checked[row * 5 + 3] &&
+      checked[row * 5 + 4]
+    );
+  }
+
+  function checkCol(col: number) {
+    return (
+      checked[col] &&
+      checked[col + 5] &&
+      checked[col + 10] &&
+      checked[col + 15] &&
+      checked[col + 20]
+    );
+  }
+
+  function checkDiagonal() {
+    return checked[0] && checked[6] && checked[18] && checked[24];
+  }
+
+  function checkAntiDiagonal() {
+    return checked[4] && checked[8] && checked[16] && checked[20];
+  }
+
+  function toggle(index: number) {
+    checked[index] = !checked[index];
+    checkFringo();
+
+    saveState();
+  }
+
+  function saveState() {
+    localStorage.setItem("saveDay", now.toString(36));
+    localStorage.setItem("checked", JSON.stringify(checked));
+  }
+
+  export function reset() {
+    checked = new Array(25)
+      .fill(false)
+      .map((_, i) => i == 12 || board[i] == "Gay");
+    localStorage.removeItem("checked");
+    localStorage.removeItem("saveDay");
+    isFringo = false;
+    fringoCount = 0;
+    fringoIndexes = [];
+  }
+
+  checkFringo();
+</script>
+
+<div class="board">
+  {#each board.slice(0, 12) as text, index (text)}
+    <button
+      disabled={isFringo && !fringoIndexes.includes(index)}
+      class="field"
+      onclick={() => toggle(index)}
+      class:checked={checked[index]}
+      class:gay={text == "Gay"}
+    >
+      {text}
+    </button>
+  {/each}
+  <img src={image} alt="Funny Fringo" class="field" />
+  {#each board.slice(13) as text, index (text)}
+    <button
+      disabled={isFringo && !fringoIndexes.includes(index + 13)}
+      class="field"
+      onclick={() => toggle(index + 13)}
+      class:checked={checked[index + 13]}
+      class:gay={text == "Gay"}
+    >
+      {text}
+    </button>
+  {/each}
+</div>
