f8ce86fe6b3557bd6bf97bb3f2c591c2e22604d4
LeBogo <lebogomc@gmail.com>
2025-01-29 01:47:49 +0100

    add SCSS support and split up styles into separate files

--- src/lib/ThemeToggle.svelte
+++ src/lib/ThemeToggle.svelte
@@ -8,28 +8,19 @@
 </script>
 
 {#if theme == "light"}
-  <button aria-label="Light mode" onclick={() => (theme = "dark")}>
+  <button
+    class="theme-toggle"
+    aria-label="Light mode"
+    onclick={() => (theme = "dark")}
+  >
     <i class="ri-sun-fill"></i>
   </button>
 {:else}
-  <button aria-label="Dark mode" onclick={() => (theme = "light")}>
+  <button
+    class="theme-toggle"
+    aria-label="Dark mode"
+    onclick={() => (theme = "light")}
+  >
     <i class="ri-moon-fill"></i>
   </button>
 {/if}
-
-<style>
-  button {
-    position: absolute;
-    top: 0;
-    right: 0;
-    background: none;
-    border: none;
-    cursor: pointer;
-    font-size: 3rem;
-    padding: 0.5rem;
-  }
-
-  .ri-moon-fill {
-    color: white;
-  }
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { mount } from 'svelte'
 import 'remixicon/fonts/remixicon.css'
-import './app.css'
+import './app.scss'
+import './css/field.scss'
+import './css/button.scss'
 import App from './App.svelte'
 const app = mount(App, {
   target: document.getElementById('app'),
